fd581f7f389fdbb93ab5cd5c3a62cce87b0a3288
Vijay97551 <56202159+Vijay97551@users.noreply.github.com>
2021-10-20 09:42:30 +0530

    Create kadane_algo.cpp

---
+++ kadane_algo.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for (int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    int max_so_far = a[0];
+    int curr=a[0];
+    for(int i=1; i<n; i++)
+    {
+        curr= max(a[i],curr+a[i]);
+        max_so_far = max(curr,max_so_far);
+    }
+    cout<<endl<<max_so_far;
+
+
+
+}
